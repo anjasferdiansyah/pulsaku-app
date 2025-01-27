dbb0cb11aa0907a3df1b1655a9f5cf3907186f2f
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2025-01-27 11:46:01 +0700

    finalize

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:testing_flutter/config/app_router.dart';
-import 'package:testing_flutter/config/database_helper.dart';
-
 import 'package:testing_flutter/core/di/app_container.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 void main() async {
-
-WidgetsFlutterBinding.ensureInitialized();
-
-  final dbHelper = DatabaseHelper();
-
-
-  await dbHelper.deleteExistingDatabase();
-
-  // // Seed the transactions
-  //   final transactionStorage = TransactionLocalStorage(dbHelper);
-  //   final customerStorage = CustomerLocalStorage(dbHelper);
-
-  // await customerStorage.seedData();
-  // await transactionStorage.seedTransactions();
-  
-
   setup();
   runApp(const ProviderScope(
     child: MyApp(),
